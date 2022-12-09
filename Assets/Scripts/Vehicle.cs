552b2d3eea4968ceca70c71dd2a148caf6bd1249
Adam <leftoverenchilada@gmail.com>
2022-12-08 22:28:27 -0500

    Wheel & Vehicle Rotation now Match.

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -18,7 +18,8 @@ namespace Burnout
 
         private void Start()
         {
-            for (int i = 0; i < wheelCount; i++) { wheels[i].Init(this); }
+            for(int i = 0; i < wheelCount; i++)
+            { wheels[i].Init(this); }
         }
 
         private void FixedUpdate()
@@ -26,61 +27,62 @@ namespace Burnout
             // Gravity
             body.velocity = new Vector3(body.velocity.x, Mathf.Clamp(body.velocity.y, -54f, 54f), body.velocity.z);
 
-            foreach (var obj in wheels) { obj.FixedUpdate(); }
+            foreach(var obj in wheels)
+            { obj.FixedUpdate(); }
         }
 
         private void Update()
         {
             input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-            foreach (var obj in wheels) { obj.Update(); }
+            foreach(var obj in wheels)
+            { obj.Update(); }
         }
 
         private void OnValidate()
         {
-            foreach (var obj in wheels) { obj.UpdateValues(); }
+            foreach(var obj in wheels)
+            { obj.UpdateValues(); }
         }
 
         private void OnDrawGizmos()
         {
-            foreach (var obj in wheels)
+            foreach(var obj in wheels)
             {
-                //Gizmos.color = Color.red;
-                //Gizmos.DrawWireSphere(obj.transform.position + new Vector3(0f, spring.maxRange + wheelRadius, 0f), 0.1f);
-                //Gizmos.DrawWireSphere(obj.transform.position - new Vector3(0f, spring.minRange + wheelRadius, 0f), 0.1f);
+                float quantity = 10;
+                float degrees = 135;
 
-                //Gizmos.color = Color.white;
-                //Gizmos.DrawLine(obj.transform.position + new Vector3(0f, spring.maxRange, 0f), obj.transform.position - new Vector3(0f, spring.minRange, 0f));
+                degrees = Mathf.Clamp(degrees, 0, 360); // Arcs/Circles are limited to 0-360 degrees.
 
-                //Gizmos.color = Color.magenta;
-                //Gizmos.DrawSphere(transform.up, 0.1f);
-                //Gizmos.color = Color.blue;
-                //Gizmos.DrawSphere(transform.position, 0.1f);
+                float angleDiff = (degrees / quantity); // Degrees between each raycast.
+                float angleCurr = (angleDiff / 2);
 
-                uint amount = 45;
-                float sum = 135;
+                float offset = (angleDiff * (quantity / 2)) + (180 - degrees); // MAGIC!
 
-                sum = Mathf.Clamp(sum, 0, 360); // Bad things...
-
-                float angle = (sum / amount);
-                float currentAngle = (angle / 2);
-
-                var offset = (angle * (amount / 2f)) + (180 - sum);
-                for (uint i = 0; i < amount; i++)
+                for(uint i = 0; i < quantity; i++)
                 {
-                    var c = (float)i / amount;
-                    if (c < 0.25f) Gizmos.color = new Color(1, 0, 0, 1);
-                    else if (c < 0.50f) Gizmos.color = new Color(0, 1, 0, 1);
-                    else if (c < 0.75f) Gizmos.color = new Color(0, 0, 1, 1);
-                    else if (c < 1.00f) Gizmos.color = new Color(1, 1, 1, 1);
+                    float c = i / quantity;
+                    if     (c < 0.25f) { Gizmos.color = new Color(1, 0, 0, 1); }
+                    else if(c < 0.50f) { Gizmos.color = new Color(0, 1, 0, 1); }
+                    else if(c < 0.75f) { Gizmos.color = new Color(0, 0, 1, 1); }
+                    else if(c < 1.00f) { Gizmos.color = new Color(1, 1, 1, 1); }
 
-                    Vector3 rot = transform.rotation.eulerAngles;
-                    Quaternion dir = Quaternion.Euler(currentAngle + offset, rot.y, -rot.z);
+                    Quaternion dir = Quaternion.AngleAxis(angleCurr + offset, transform.right); // Pure hatred for Quaternions.
 
-                    Gizmos.DrawRay(new Ray(obj.transform.position, dir * transform.up));
+                    Gizmos.DrawRay(obj.transform.position, dir * transform.up);
 
-                    currentAngle += angle;
+                    angleCurr += angleDiff;
                 }
+
+
+                /*
+                Gizmos.color = Color.magenta;
+                Gizmos.DrawWireSphere(obj.transform.position + new Vector3(0f, spring.maxRange + wheelRadius, 0f), 0.1f);
+                Gizmos.DrawWireSphere(obj.transform.position - new Vector3(0f, spring.minRange + wheelRadius, 0f), 0.1f);
+
+                Gizmos.color = Color.gray;
+                Gizmos.DrawLine(obj.transform.position + new Vector3(0f, spring.maxRange, 0f), obj.transform.position - new Vector3(0f, spring.minRange, 0f));
+                */
             }
         }
     }
