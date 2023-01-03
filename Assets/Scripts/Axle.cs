499ae7b516c76b8ae4d61ab0f998e3003f7e6b4b
Adam <70708407+23calva@users.noreply.github.com>
2023-01-03 12:27:13 -0500

    ...

--- Assets/Scripts/Axle.cs
+++ Assets/Scripts/Axle.cs
@@ -1,17 +1,26 @@
-using System;
 using UnityEngine;
 
 namespace Burnout
 {
-    [Serializable]
-    public class Axle
+    public class Axle : MonoBehaviour
     {
         public Wheel leftWheel;
         public Wheel rightWheel;
 
         public float antiRollStrength = 1;
 
-        public void FixedUpdate()
+        public float localWheelPosition;
+
+        private void Awake()
+        {
+            leftWheel = this.gameObject.AddComponent<Wheel>();
+            rightWheel = this.gameObject.AddComponent<Wheel>();
+
+            leftWheel.transform.localPosition = transform.localPosition + (Vector3.left * localWheelPosition);
+            rightWheel.transform.localPosition = transform.localPosition + (Vector3.right * localWheelPosition);
+        }
+
+        private void FixedUpdate()
         {
             Vector3 antiRollForce = Vector3.up * ((leftWheel.spring.offset - rightWheel.spring.offset) * antiRollStrength);
 
--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -15,12 +15,18 @@ namespace Burnout
         public GameObject wheelPrefab;
         public float wheelRadius;
 
+        private void Start()
+        {
+            axles[0] = gameObject.AddComponent<Axle>();
+            axles[0].transform.localPosition = new Vector3(0f, 0.5f, 1.95f);
+            axles[0].localWheelPosition = 0.85f;
+            axles[1] = gameObject.AddComponent<Axle>();
+            axles[1].transform.localPosition = new Vector3(0f, 0.5f, 1.85f);
+            axles[1].localWheelPosition = 0.85f;
+        }
+
         private void FixedUpdate()
         {
-            foreach(var axle in axles)
-            {
-                axle.FixedUpdate();
-            }
         }
 
         private void Update()
