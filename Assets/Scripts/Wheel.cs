1e5e075bd380781dc4b8c8f3926917fc21af2a55
Adam <70708407+23calva@users.noreply.github.com>
2023-01-09 12:27:13 -0500

    Wheel Algorithm Change
    Use SphereCast instead of janky Raycasts. This may have fixed everything, test to make sure.

--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -17,13 +17,57 @@ namespace Burnout
 
         public void ApplySpringForce(Vector3 additional = default)
         {
-            additional += GetSpringForce(180, 10);
+            //additional += GetSpringForce(180, 10);
+            additional += GetSpringForce();
             Veh.Body.AddForceAtPosition(Veh.transform.TransformVector(additional), Position, ForceMode.Acceleration);
+        }
+
+        RaycastHit? Hit;
+        public Vector3 GetSpringForce()
+        {
+            RaycastHit[] contacts = Physics.SphereCastAll(Position, 0, Vector3.down, Veh.WheelRadius); // Radius and MaxDistance are misleading.
+            Hit = null;
+
+            // Check for the bottom-most contact and that is Spring.Offset -> Using contact.normal convert to direction vector3 then calculate force.
+            float distance = float.MaxValue;
+            for(int i = 0; i < contacts.Length; i++)
+            {
+                if (contacts[i].distance < distance)
+                {
+                    distance = contacts[i].distance;
+                    Hit = contacts[i];
+                }
+            }
+
+            if (Hit.HasValue)
+            {
+                Vector3 velocity = Veh.Body.GetPointVelocity(Position);
+
+                Spring.offset = Veh.Spring.restLength - Hit.Value.distance;
+
+                float force = (Spring.offset * Veh.Spring.strength) - (velocity.y * Veh.Spring.damping);
+                return new Vector3(0, force, 0);
+            }
+
+            return Vector3.zero;
+        }
+
+        public void DrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireSphere(Position, Veh.WheelRadius);
+
+            if (Hit.HasValue)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawRay(Position, Veh.transform.up - Hit.Value.normal * 10);
+            }
         }
 
         // I believe raycasts are the wrong way to go...
         //https://docs.unity3d.com/ScriptReference/Physics.CapsuleCast.html
         //http://answers.unity.com/answers/1781142/view.html
+        /*
         private Vector3 GetSpringForce(float degrees, float iterations)
         {
             degrees = Mathf.Clamp(degrees, 0, 360); // Arcs/circles are limited to 0-360 degrees.
@@ -86,5 +130,6 @@ namespace Burnout
                 angleCurr += angleDiff;
             }
         }
+        */
     }
 }
